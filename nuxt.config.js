ded458864004b6204dbc2458364dd9079d748177
Jonas Galvez <jonasgalvez@gmail.com>
2019-09-28 18:40:59 -0300

    turn into module

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,20 @@
 export default {
-  plugins: ['~/plugins/json-data']
+  modules: [
+    function () {
+      const renderer = this.nuxt.renderer
+      const renderRoute = renderer.renderRoute.bind(renderer)
+      renderer.renderRoute = function() {
+        const renderScripts = this.serverContext.renderScripts
+        const nuxtState = this.serverContext.nuxt
+        this.serverContext.renderScripts = function () {
+          return `${
+            renderScripts(arguments)
+          }<script type="application/json" id="__NUXT_JSON_DATA__">${
+            JSON.stringify(nuxtState)
+          }</script>`
+        }
+        return renderRoute(arguments)
+      }
+    }
+  ]
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,11 @@
 <template>
-  <p>{{ $jsonData.msg }}</p>
+  <p>{{ msg }}</p>
 </template>
 
 <script>
-function jsonData(vm, data) {
-  vm.$ssrContext.jsonData = data
-  const oldRenderScripts = vm.$ssrContext.renderScripts
-  vm.$ssrContext.renderScripts = function () {
-    return `${oldRenderScripts(arguments)}<script type="application/json" id="__NUXT_JSON_DATA__">${
-      JSON.stringify(data)}</` + `script>`
-  }
-}
-
 export default {
-  serverPrefetch () {
-    jsonData(this, {
+  asyncData: () => ({
       msg: 'Hello world!'
-    })
-  }
+  })
 }
 </script>
